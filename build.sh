fcce31a8e6e4f63d4f6fb1fa3888ff41d66e16a3
Nicholas Musco <nmusco@Nicholass-MBP-2.intakedesk.com>
2020-12-22 13:48:31 -0500

    Minor update

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sed "-i" "" "-e" 's/    rate estimate:/    rate_estimate:/g' "openapi.yaml"
 openapi-generator generate -g php -c config.yaml --http-user-agent "nmusco/shipengine-sdk-php-v1" -i openapi.yaml
 sed "-i" "" "-e" 's/\\\\Nmusco\\ShipEngine\\v1\\Models\\\\/\\\\Nmusco\\\\ShipEngine\\\\v1\\\\Models\\\\/g' "lib/ObjectSerializer.php"
 sed "-i" "" "-e" 's/\\\\Nmusco\\ShipEngine\\v1\\\\/\\\\Nmusco\\\\ShipEngine\\\\v1\\\\/g' "lib/ObjectSerializer.php"

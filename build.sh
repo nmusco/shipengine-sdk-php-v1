ba63313514f901daa11725c8ea3057eaf35dfc8a
Nicholas Musco <nmusco@Nicholass-MBP-2.intakedesk.com>
2020-12-22 13:59:44 -0500

    Minor update

--- build.sh
+++ build.sh
@@ -3,15 +3,4 @@ sed "-i" "" "-e" 's/    rate estimate:/    rate_estimate:/g' "openapi.yaml"
 openapi-generator generate -g php -c config.yaml --http-user-agent "nmusco/shipengine-sdk-php-v1" -i openapi.yaml
 sed "-i" "" "-e" 's/\\\\Nmusco\\ShipEngine\\v1\\Models\\\\/\\\\Nmusco\\\\ShipEngine\\\\v1\\\\Models\\\\/g' "lib/ObjectSerializer.php"
 sed "-i" "" "-e" 's/\\\\Nmusco\\ShipEngine\\v1\\\\/\\\\Nmusco\\\\ShipEngine\\\\v1\\\\/g' "lib/ObjectSerializer.php"
-# for file in lib/Models/*.php
-# do
-# 	sed "-i" "" "-e" 's/\\Nmusco\\ShipEngine\\v1\\Models\\\\Nmusco\\ShipEngine\\v1\\Models\\/\\Nmusco\\ShipEngine\\v1\\Models\\/g' "$file"
-# 	sed "-i" "" "-e" 's/\\Nmusco\\ShipEngine\\v1\\Models\\\\DateTime/\\DateTime/g' "$file"
-# done
-
-# for file in lib/Api/*.php
-# do
-# 	sed "-i" "" "-e" 's/\\Nmusco\\ShipEngine\\v1\\Models\\\\Nmusco\\ShipEngine\\v1\\Models\\/\\Nmusco\\ShipEngine\\v1\\Models\\/g' "$file"
-# 	sed "-i" "" "-e" 's/\\Nmusco\\ShipEngine\\v1\\Models\\\\DateTime/\\DateTime/g' "$file"
-# done
 ./git_push.sh
